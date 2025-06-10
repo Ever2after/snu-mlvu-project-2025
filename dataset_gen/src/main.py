0a2f081581542593152fc913a88138adf1c995d7
jsppa <102790497+jsppa@users.noreply.github.com>
2025-06-11 06:47:38 +0900

    obj_moving conf

--- dataset_gen/src/main.py
+++ dataset_gen/src/main.py
@@ -86,7 +86,7 @@ GEN_SCRIPT = os.path.abspath("generate_scene.py")
 BAKE_SCRIPT = os.path.abspath("bake_scene.py")
 RENDER_SCRIPT = os.path.abspath("render_scene.py")
 
-mode = "all" # ["scene_gen_only", "bake", "render", "all"]
+mode = "bake" # ["scene_gen_only", "bake", "render", "all"]
 # "scene_gen_only": generate scenes and do not bake/render
 # "bake": generate scenes, bake but not render
 # "render": render scene given .blend file and fluid cache
@@ -122,7 +122,6 @@ for i in os.listdir("../scene"):
 
     for idx in range(len(sampler)):
         sample = sampler[idx]
-        if sample["viscosity"] != 0.02 or sample["flow_size"] != (1.5, 1.5, 1.5) or sample["flow_loc"] != (0.5, 0.5, 0): continue
         print(f"current: {idx + 1}/{len(sampler)}")
         for n in sample:
             print(f"\t{n}: {'Default' if sample[n] is None else sample[n]}")
@@ -150,7 +149,7 @@ for i in os.listdir("../scene"):
                 # bake scene only when cache does not exist
                 execute_bake(BLENDER_PATH, scene_aug_path, BAKE_SCRIPT, cache_dir)
         
-        # 3. render scene
+        # 4. render scene
         if mode in ["render", "all"]:
             # copy generated fluid cache
             if cache_dir is None:
